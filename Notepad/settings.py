ef5085443a5fbf04a9647de53de7ed5e0fc676a9
Anuj-negi2207 <pienegi22@gmail.com>
2022-12-08 05:17:12 +0530

    all as host

--- Notepad/settings.py
+++ Notepad/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-2(&$%lcd=sqa@ca#%jp*21th4i($1vb(6-ta^yu6g_q^wb-zw7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
